271bfc469c632d9da1bda83b5164bca69019255b
Juleshpun <julesh.pun@giksindia.com>
2023-01-24 12:03:34 +0530

    changes-in-feature-product

--- src/components/Admin/NewOrder.js
+++ src/components/Admin/NewOrder.js
@@ -174,7 +174,7 @@ const NewOrder=(props)=>{
          </div>
          <div className='col-10'>
         {/* login Products Details Modal  */}
-   <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+   <div className="modal-1 fade-1" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog justify-content-center d-grid">
          <div className="modal-content order-details-div">
             <div className="modal-body ">
