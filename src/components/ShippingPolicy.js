cffa6700b8950e7b29f07fb9455cb30ae9fa31a1
Juleshpun <julesh.pun@giksindia.com>
2023-01-24 18:29:25 +0530

    created-footer-pages

---
+++ src/components/ShippingPolicy.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Footer from './Footer';
+import Header1 from './Header1';
+import '../components/ShippingPolicy.css';
+
+function ShippingPolicy() {
+  return (
+    <>
+    <Header1/>
+    <div className='container m-auto'>
+        <div className='row'>
+            <div className='col-12'>
+                <div className='shipping-policy-page'>
+                    <h1>SHIPPING POLICY</h1>
+                    <p>http://www.nutrazik.giksindia.com is committed to excellence, and the full satisfaction of our
+customers. http://www.nutrazik.giksindia.com proudly offers shipping services. Be assured we are
+doing everything in our power to get your order to you as soon as possible. Please consider any
+holidays that might impact delivery times. http://www.nutrazik.giksindia.com also offers same day
+dispatch.</p>
+<h2>SHIPPING</h2>
+<p>All orders for our products are processed and shipped out in 4-10 business days. Orders are not
+shipped or delivered on weekends or holidays. If we are experiencing a high volume of orders,
+shipments may be delayed by a few days. Please allow additional days in transit for delivery. If there
+will be a significant delay in the shipment of your order, we will contact you via email.</p>
+<h2>WRONG ADDRESS DISCLAIMER</h2>
+<p>It is the responsibility of the customers to make sure that the shipping address entered is correct.
+We do our best to speed up processing and shipping time, so there is always a small window to
+correct an incorrect shipping address. Please contact us immediately if you believe you have
+provided an incorrect shipping address.</p>
+<h2>UNDELIVERABLE ORDERS</h2>
+<p>Orders that are returned to us as undeliverable because of incorrect shipping information are
+subject to a restocking fee to be determined by us.</p>
+<h2>LOST/STOLEN PACKAGES</h2>
+<p>http://www.nutrazik.giksindia.com is not responsible for lost or stolen packages. If your tracking
+information states that your package was delivered to your address and you have not received it,
+please report to the local authorities.</p>
+<h2>RETURN REQUEST DAYS</h2>
+<p>http://www.nutrazik.giksindia.com allows you to return its item (s) within a period of 7 days. Kindly
+be advised that the item (s) should be returned unopened and unused.
+</p>
+<h2>OUT OF STOCK ITEM PROCESS</h2>
+<p>http://www.nutrazik.giksindia.com has the following options in the event there are items which are
+out of stock http://www.nutrazik.giksindia.com Wait for all items to be in stock before dispatching.</p>
+<h2>IMPORT DUTY AND TAXES</h2>
+<p>When dealing with http://www.nutrazik.giksindia.com you have the following options when it comes
+to taxes as well as import duties:You will be required to settle the requisite fees when the items are
+arriving in the destination country.
+</p>
+<h2>ACCEPTANCE</h2>
+<p>By accessing our site and placing an order you have willingly accepted the terms of this Shipping
+Policy.</p>
+<h2>CONTACT INFORMATION</h2>
+<p>In the event you have any questions or comments please reach us via the following contacts:
+Email - Email : info@ishaanagroup.com
+</p>
+                </div>
+            </div>
+        </div>
+    <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default ShippingPolicy
