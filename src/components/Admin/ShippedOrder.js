dc164e70dfa7991ce2f697f637c34938dab5a430
Sandeep Bisht <>
2023-02-15 11:23:16 +0530

    handle hipping form

--- src/components/Admin/ShippedOrder.js
+++ src/components/Admin/ShippedOrder.js
@@ -1,53 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import DataTable from '@bit/adeoy.utils.data-table';
-import Sidemenu from './Sidemenu';
-import './Dashboard.css';
-import { baseUrl } from '../../utils/services';
-import DashboardHeaader from './DashboardHeaader';
-import { Table, Input, Space, Popconfirm, Typography,Dropdown,Modal } from "antd";
+import Sidemenu from "./Sidemenu";
+import "./Dashboard.css";
+import { baseUrl } from "../../utils/services";
+import DashboardHeaader from "./DashboardHeaader";
+import {
+  Table,
+  Input,
+  Space,
+  Popconfirm,
+  Typography,
+  Dropdown,
+  Modal,
+} from "antd";
 import { BiSearchAlt } from "react-icons/bi";
-import {MdPlaylistAdd} from 'react-icons/md'
-import {Link} from "react-router-dom";
-import { DownOutlined } from '@ant-design/icons';
+import { MdPlaylistAdd } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { DownOutlined } from "@ant-design/icons";
 
 const ShippedOrder = () => {
-  const [orders, setOrders] = useState([])
-  const [OrderDetails, setOrderDetails] = useState([])
+  const [orders, setOrders] = useState([]);
+  const [OrderDetails, setOrderDetails] = useState([]);
   const [filteredData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchVal, setSearchVal] = useState("");
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [shippedOrder,setShippedOrder]=useState([]);
-  const [orderItem,setOrderItem]=useState([]);
+  const [shippedOrder, setShippedOrder] = useState([]);
+  const [orderItem, setOrderItem] = useState([]);
   useEffect(() => {
     GetOrders();
   }, []);
 
   const GetOrders = async () => {
-
     await fetch(`${baseUrl}/api/order/all_order`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(async (data) => {
-        let arr=[];
-         for(let item of data.data)
-         {
-            if(item.status=="Shipped")
-
-            {
-               arr.push(item);
-            }
-         }
-         setOrderDetails(arr)
-        
-      }
-      )
+        let arr = [];
+        for (let item of data.data) {
+          if (item.status == "Shipped") {
+            arr.push(item);
+          }
+        }
+        setOrderDetails(arr);
+      })
       .catch((err) => {
         console.log(err, "errors");
       });
-  }
+  };
 
-  const UpdateOrderStatus = async (e,orderId, status) => {
-e.preventDefault();
+  const UpdateOrderStatus = async (e, orderId, status) => {
+    e.preventDefault();
     await fetch(`${baseUrl}/api/order/update_order`, {
       method: "PATCH",
       headers: {
@@ -61,7 +63,7 @@ e.preventDefault();
     })
       .then((res) => res.json())
       .then(async (data) => {
-         GetOrders();
+        GetOrders();
         setIsModalVisible(false);
       })
       .catch((err) => {
@@ -81,9 +83,7 @@ e.preventDefault();
     })
       .then((res) => res.json())
       .then(async (data) => {
-
         GetOrders();
-
       })
       .catch((err) => {
         console.log(err, "error");
@@ -91,9 +91,8 @@ e.preventDefault();
   };
 
   const CaptureDetails = (orders) => {
-    setOrderDetails(orders)
-
-  }
+    setOrderDetails(orders);
+  };
   const onChangeHandler = (e) => {
     setSearchVal(e.target.value);
     if (e.target.value === "") {
@@ -106,75 +105,148 @@ e.preventDefault();
     });
     setOrders(filteredData);
   };
-  
-  
 
   const showModal = (item) => {
     setShippedOrder(item.address);
     setOrderItem(item);
     setIsModalVisible(true);
-    };
-
+  };
 
   const columns = [
     { title: "Order No.", dataIndex: "order_no", key: "order_no" },
     { title: "Paid Amount.", dataIndex: "totalamount", key: "totalamount" },
-    { title: "Payment Status", dataIndex: "payment_status", key: "payment_status" },
     {
-        title: 'Status',
-        key: 'action',
-        render: (_,item) => (
-              <a
-          onClick={()=>showModal(item)}>
-            Shipped
-          </a> 
-        ),
-      },
+      title: "Payment Status",
+      dataIndex: "payment_status",
+      key: "payment_status",
+    },
+    {
+      title: "Status",
+      key: "action",
+      render: (_, item) => <a onClick={() => showModal(item)}>Shipped</a>,
+    },
   ];
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <>
-    {/* table modal */}
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header float-right">
-        <h5>User details</h5>
-        <div class="text-right">
-          <i data-dismiss="modal" aria-label="Close" class="fa fa-close"></i>
-        </div>
+      {/* table modal */}
+      
+       
+            <div class="modal-body">
+              <div>
+                <Modal title="Shipped Details" aria-hidden="true" visible={isModalVisible} onOk={handleOk}
+                  onCancel={handleCancel}>
+                  <form>
+                    <div className="row">
+                    <div class="col-md-4 mb-3">
+                      <label for="line1" class="form-label">
+                        Line 1
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="line1"
+                        value={shippedOrder.line1}
+                        //aria-describedby="emailHelp"
+                      />
+                    </div>
+                    { shippedOrder && shippedOrder.line2 &&
+                    <div class="col-md-4 mb-3">
+                      <label for="line2" class="form-label">
+                        Line 2
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="line2"
+                        value={shippedOrder.line2}
+                        //aria-describedby="emailHelp"
+                      />
+                    </div>
+                    }
+                    <div class="col-md-4 mb-3">
+                      <label for="city" class="form-label">
+                        City
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="city"
+                        value={shippedOrder.city}
+                        //aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                      <label for="state" class="form-label">
+                        State
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="state"
+                        value={shippedOrder.state}
+                        //aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div className="col-md-4">
+                      <label className="form-label">Postal Code</label>
+                      <input
+                      type="text"
+                      className="form-control"
+                      value={shippedOrder.postal_code}
+                      //aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                      <label for="country" class="form-label">
+                        Country
+                      </label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="country"
+                        value={shippedOrder.country}
+                        aria-describedby="emailHelp"
+                      />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                      <label for="Blue Dart" class="form-label">Choose a Shipper</label>
+                      <select className="m-1 form-control">
+                        <option value="Blue Dart">Blue Dart</option>
+                        <option value="XpressBees">XpressBees</option>
+                        <option value="DHL Shipping">DHL Shipping</option>
+                        <option value="DTDC Courier">DTDC Courier</option>
+                      </select>
+                    </div>
+                    </div>
+                    <button
+                      className="btn btn-primary m-2"
+                      onClick={(e) =>
+                        UpdateOrderStatus(e, orderItem._id, "Delivered")
+                      }
+                    >
+                      Delivered
+                    </button>
+                    <button
+                      className="btn btn-primary m-2"
+                      onClick={(e) =>
+                        UpdateOrderStatus(e, orderItem._id, "Cancel")
+                      }
+                    >
+                      Cancle
+                    </button>
+                    
+                  </form>
+                </Modal>
+              </div>
+            
       </div>
-      <div class="modal-body">
-        <div>       
-        <Modal title="Shipped Details" visible={isModalVisible}>
-        <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Address</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" value={shippedOrder.line1} aria-describedby="emailHelp" />
-  </div>
-  <div class="mb-3">
-  <label for="Blue Dart">Choose a Shipper</label>
-    <select className='m-1'>
-      <option value="Blue Dart">Blue Dart</option>
-      <option value="XpressBees">XpressBees</option>
-      <option value="DHL Shipping">DHL Shipping</option>
-      <option value="DTDC Courier">DTDC Courier</option>
-    </select>
-  </div>
-  <button className='btn btn-primary m-2' onClick={(e)=>UpdateOrderStatus(e,orderItem._id,"Delivered")}>Delivered</button>
-  <button className='btn btn-primary m-2'onClick={(e)=>UpdateOrderStatus(e,orderItem._id,"Cancel")}>Cancle</button>
-  {/* <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div> */}
-</form>
-                    </Modal> 
- </div>
-      </div>
-    </div>
-  </div>
-</div>
       {/* end modal */}
       <section id="body-pd">
         <div className="container-fluid">
@@ -186,25 +258,28 @@ e.preventDefault();
             <div className="col-10">
               <div className="category-details-section">
                 <h3 className="all-category-head">Orders </h3>
-                <div className="all-category-search-wrap">
-                  {/* <Link to="/Category" className="add-icon">
-                    <MdPlaylistAdd />Add
-                  </Link> */}
+                <div className="all-category-search-wrap">                  
                   <input
-                    type='text'
-                    onChange={e => onChangeHandler(e)}
+                    type="text"
+                    onChange={(e) => onChangeHandler(e)}
                     onKeyUp={searchHandler}
                     placeholder="Search.."
                     enterButton
                     style={{ position: "sticky", top: "0", left: "0" }}
                   />
-                  <button type="button" className="dashboard-search-btn"><BiSearchAlt /></button>
+                  <button type="button" className="dashboard-search-btn">
+                    <BiSearchAlt />
+                  </button>
                 </div>
               </div>
 
               <Table
                 rowKey="name"
-                dataSource={filteredData && filteredData.length ? filteredData : OrderDetails}
+                dataSource={
+                  filteredData && filteredData.length
+                    ? filteredData
+                    : OrderDetails
+                }
                 columns={columns}
                 loading={loading}
                 pagination={false}
@@ -213,27 +288,8 @@ e.preventDefault();
           </div>
         </div>
       </section>
-      <div class="modal" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
     </>
-  );
-}
+  )
+};
 
 export default ShippedOrder;
